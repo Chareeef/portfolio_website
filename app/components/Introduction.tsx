9de060e5e3b239009ffd9806faa2f56b15a284b0
Chareeef <youssef.charif.h@gmail.com>
2024-12-29 23:36:07 +0100

    fix picture

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -9,28 +9,26 @@ const Introduction = () => {
 
   return (
     <section className="py-20 text-center">
-      <div className="mb-8 relative">
-        <div className="w-48 h-48 mx-auto rounded-full overflow-hidden bg-white dark:bg-gray-800">
-          <Image
-            src="/my_picture.png"
-            alt="Youssef Charif Hamidi"
-            width={200}
-            height={200}
-            className="rounded-full mx-auto"
-          />
-        </div>
+      <div className="relative mb-8">
+        <Image
+          src="/my_picture.png"
+          alt="Youssef Charif Hamidi"
+          width={200}
+          height={200}
+          className="mx-auto h-48 w-48 overflow-hidden rounded-full bg-white dark:bg-gray-800"
+        />
       </div>
       <h2
-        className="text-4xl md:text-6xl font-bold mb-6 relative inline-block"
+        className="relative mb-6 inline-block text-4xl font-bold md:text-6xl"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         Welcome to My Cosmic Journey
         <Rocket
-          className={`inline-block ml-2 transition-transform duration-300 ${isHovered ? "transform translate-x-2 -translate-y-2" : ""}`}
+          className={`ml-2 inline-block transition-transform duration-300 ${isHovered ? "-translate-y-2 translate-x-2 transform" : ""}`}
         />
       </h2>
-      <p className="text-xl md:text-2xl max-w-3xl mx-auto">
+      <p className="mx-auto max-w-3xl text-xl md:text-2xl">
         Greetings, fellow space traveler! I am a passionate software engineer
         and co-founder of RemoteOtter, a platform connecting professionals with
         remote job opportunities. My journey, marked by resilience and creative

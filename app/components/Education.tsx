77d21d4a8c675006944653e0e933aed4e039dfe2
Chareeef <youssef.charif.h@gmail.com>
2024-12-29 02:31:14 +0100

    Add margin

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -108,7 +108,7 @@ const Education = () => {
           href={education.certificateLink}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:hidden"
+          className="mb-4 inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:hidden"
           onClick={(e) => e.stopPropagation()}
         >
           <Certificate className="mr-2 h-4 w-4" />
